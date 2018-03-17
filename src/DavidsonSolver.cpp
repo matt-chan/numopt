e258c6d2e2e2c2bc8b5db2cdee8c8628acb4477b
Laurent Lemmens <laurent.lemmens@ugent.be>
2018-03-17 14:04:50 -0400

    Implement getters.

--- src/DavidsonSolver.cpp
+++ src/DavidsonSolver.cpp
@@ -5,12 +5,28 @@
 namespace numopt {
 
 
-double DavidsonSolver::get_eigenvalue() const { return this->eigenvalue; }
-Eigen::VectorXd DavidsonSolver::get_eigenvector() const { return this->eigenvector; }
+/*
+ *  GETTERS
+ */
+double DavidsonSolver::get_eigenvalue() const {
+
+    if (this->is_solved) {
+        return this->eigenvalue;
+    } else {
+        throw std::runtime_error("You are trying to get an eigenvalue but the solution hasn't been found yet.");
+    }
+}
+
+
+Eigen::VectorXd DavidsonSolver::get_eigenvector() const {
+
+    if (this->is_solved) {
+        return this->eigenvector;
+    } else {
+        throw std::runtime_error("You are trying to get an eigenvector but the solution hasn't been found yet.");
+    }
+}
 
 
 
 }  // namespace numopt
-
-
-
