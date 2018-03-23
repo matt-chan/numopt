f17d0f7578c091ac8e1744ceb8ae1e7dc69ecff2
Laurent Lemmens <laurent.lemmens@ugent.be>
2018-03-23 06:55:47 -0400

    Minor style changes.

--- cmake/ConfigureLibrary.cmake
+++ cmake/ConfigureLibrary.cmake
@@ -1,10 +1,10 @@
-# In this CMake file, we will include the headers and link to the necessary libraries
+# In this CMake file, we will include the headers and link to the necessary libraries.
 
 
-# Include this project's headers
+# Include this project
 target_include_directories(${LIBRARY_NAME} PRIVATE ${PROJECT_INCLUDE_FOLDER})
 
-# Include Eigen3
+# Include Eigen
 target_link_libraries(${LIBRARY_NAME} PUBLIC Eigen3::Eigen)
 
 # Include Spectra
