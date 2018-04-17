b84d66243a0de2fb7bf3abb5b73bb2c245aef274
Laurent Lemmens <laurent.lemmens@ugent.be>
2018-04-17 16:13:28 -0400

    Add tests for normalized eigenvectors.

--- tests/DenseSolver_test.cpp
+++ tests/DenseSolver_test.cpp
@@ -37,4 +37,8 @@ BOOST_AUTO_TEST_CASE ( simple_dense ) {
 
     BOOST_CHECK(std::abs(test_lowest_eigenvalue - ref_lowest_eigenvalue) < 1.0e-08);
     BOOST_CHECK(cpputil::linalg::areEqualEigenvectors(test_lowest_eigenvector, ref_lowest_eigenvector, 1.0e-08));
+
+
+    // Check if the eigenvector is normalized
+    BOOST_CHECK(std::abs(dense_solver.get_eigenvector().norm() - 1) < 1.0e-12);
 }
