8de607dc9f1636c42520d8ee6134dde3d51ac569
Laurent Lemmens <laurent.lemmens@ugent.be>
2018-05-23 11:14:20 -0400

    Update copyright headers.

--- include/numopt.hpp
+++ include/numopt.hpp
@@ -2,18 +2,18 @@
 // 
 // Copyright (C) 2017-2018  the GQCG developers
 // 
-// GQCG-cpputil is free software: you can redistribute it and/or modify
+// GQCG-numopt is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 // 
-// GQCG-cpputil is distributed in the hope that it will be useful,
+// GQCG-numopt is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Lesser General Public License for more details.
 // 
 // You should have received a copy of the GNU Lesser General Public License
-// along with GQCG-cpputil.  If not, see <http://www.gnu.org/licenses/>.
+// along with GQCG-numopt.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef NUMOPT_NUMOPT_HPP
 #define NUMOPT_NUMOPT_HPP
 
