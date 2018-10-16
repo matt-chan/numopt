af4078d53bc1f20f356ca74a404504a9b765a23a
Guillaume Acke <Guillaume.Acke@UGent.be>
2018-10-16 16:48:59 +0200

    Bug fix: update warnings spectra cmake and update travis.

--- cmake/FindSpectra.cmake
+++ cmake/FindSpectra.cmake
@@ -1,7 +1,5 @@
 # FindSpectra.cmake. Find<package>.cmake-template from (https://cmake.org/Wiki/CMake:How_To_Find_Libraries#Writing_find_modules).
 
-# For the moment, only an installation of Spectra inside /usr/local is supported.
-
 # Try to find Spectra
 # Once done, this will define
 #  Spectra_FOUND            spectra is available on the system
@@ -9,12 +7,10 @@
 
 # Since it's a header-only library, the include directories are enough.
 
-
-# For the moment, only an installation in /usr/local is supported
 find_path(SPECTRA_PREFIX README.md HINTS ${CMAKE_SOURCE_DIR}/spectra /usr/local/spectra)
 
 if("${SPECTRA_PREFIX}" STREQUAL "SPECTRA_PREFIX-NOTFOUND")
-    message(WARNING "Spectra was not found in location /usr/local/.")
+    message(WARNING "Spectra was not found.")
 else()
     # If found, we let the user know that Spectra was found
     message(STATUS "Spectra was found at ${SPECTRA_PREFIX}")
